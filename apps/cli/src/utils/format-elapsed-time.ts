05232df0946b44b8d85541b0b2421d5feaac7f26
Aiden Bai <aiden.bai05@gmail.com>
2026-03-14 23:16:47 -0700

    add links

--- apps/cli/src/utils/format-elapsed-time.ts
+++ apps/cli/src/utils/format-elapsed-time.ts
@@ -1,18 +1,4 @@
-import { MS_PER_SECOND, SECONDS_PER_MINUTE } from "../constants.js";
+import prettyMs from "pretty-ms";
 
-export const formatElapsedTime = (elapsedTimeMs: number) => {
-  const totalElapsedSeconds = Math.max(0, Math.floor(elapsedTimeMs / MS_PER_SECOND));
-  const elapsedSeconds = totalElapsedSeconds % SECONDS_PER_MINUTE;
-  const totalElapsedMinutes = Math.floor(totalElapsedSeconds / SECONDS_PER_MINUTE);
-
-  if (totalElapsedMinutes < SECONDS_PER_MINUTE) {
-    return `${totalElapsedMinutes}:${elapsedSeconds.toString().padStart(2, "0")}`;
-  }
-
-  const elapsedMinutes = totalElapsedMinutes % SECONDS_PER_MINUTE;
-  const elapsedHours = Math.floor(totalElapsedMinutes / SECONDS_PER_MINUTE);
-
-  return `${elapsedHours}:${elapsedMinutes.toString().padStart(2, "0")}:${elapsedSeconds
-    .toString()
-    .padStart(2, "0")}`;
-};
+export const formatElapsedTime = (elapsedTimeMs: number): string =>
+  prettyMs(Math.max(0, elapsedTimeMs), { secondsDecimalDigits: 0 });
